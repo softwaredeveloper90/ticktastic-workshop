1efe3ecd8b905d870ace9c6ae5f8df6b3f22eb0c
Sami Woon <swoon@iamdigital.uk>
2017-02-09 17:02:24 +0000

    javascript two

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
-var $form = $('#ticket-form');
+var tt = {};
 
-$form.on('submit', function(e) {
-	var values = { }, $inputs;
-console.log($form)
-	e.preventDefault();
-	$inputs = $form.find('select, textarea, input');
-	$inputs.each(function(key, el) {
-		var name = el.name;
-		var value = el.value;
-		if (name) { 
-			values[name] = value;
-		}
+tt.form = function($form) {
+		$form.on('submit', function(e) {
+			var values = { }, $inputs;
+
+			e.preventDefault();
+			$inputs = $form.find('select, textarea, input');
+			$inputs.each(function(key, el) {
+				var name = el.name;
+				var value = el.value;
+				if (name) { 
+					values[name] = value;
+				}
+			});
+
+		window.localStorage.setItem('ticket', JSON.stringify(values));
 	});
+};
 
-	window.localStorage.setItem('ticket', JSON.stringify(values));
-})
+tt.list = function($list) {
+	var template = '<li class="ticket-list__item"><h4 class="ticket-list__title">{{ title }}</h4><span class="ticket-list__user">{{ name }}</span></li>';
 
+	var values = JSON.parse(window.localStorage.getItem('ticket'));
+
+	template = template.replace('{{ title }}', values.issue);
+	template = template.replace('{{ name }}', values.name);
+
+	$list.html(template);
+};
+
+tt.main = function() {
+	var $form = $('#ticket-form');
+	var $list = $('#ticket-list');
+
+	tt.form($form);
+	tt.list($list);
+};
+
+tt.main();
